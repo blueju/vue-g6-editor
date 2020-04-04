9e216ad789a07cb01f1d3771d1ae33396afb32ff
blueju <blueju@foxmail.com>
2020-04-04 12:01:02 +0800

    添加 eslint 和 prettier

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: ["alloy", "alloy/vue"],
+  env: {
+    // 你的环境变量（包含多个预定义的全局变量）
+    //
+    // browser: true,
+    // node: true,
+    // mocha: true,
+    // jest: true,
+    // jquery: true
+  },
+  globals: {
+    // 你的全局变量（设置为 false 表示它不允许被重新赋值）
+    //
+    // myGlobal: false
+  },
+  rules: {
+    // 自定义你的规则
+  }
+};
