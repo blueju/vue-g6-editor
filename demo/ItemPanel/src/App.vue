6378962500cbf0955bf3f905fff0575faa0829bb
blueju <blueju@foxmail.com>
2020-09-20 11:46:24 +0800

    add demo itempanel

---
+++ demo/ItemPanel/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" width="25%" />
+    <ItemPanel />
+  </div>
+</template>
+
+<script>
+import ItemPanel from "./components/ItemPanel";
+
+export default {
+  name: "App",
+  components: {
+    ItemPanel,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
